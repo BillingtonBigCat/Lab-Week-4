539ec59db7dfb89fed1592b3158af02338f84712
BillingtonBigCat <69779148+BillingtonBigCat@users.noreply.github.com>
2020-08-17 18:55:38 +1000

    100 percent complete

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class PrintAndHide : MonoBehaviour
 {
     int i = 0;
-    public Renderer rend; 
+    public Renderer rend;
+    private int random;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+          random = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -17,5 +21,16 @@ public class PrintAndHide : MonoBehaviour
     {
         ++i;
         Debug.Log(gameObject.name + ": " + i);
+
+        if (i == 100 && gameObject.CompareTag("Red"))
+        {
+            rend.enabled = false;
+        }
+
+        else if (i == random && gameObject.CompareTag("Blue"))
+        {
+            rend.enabled = false;
+        }
+
     }
 }
