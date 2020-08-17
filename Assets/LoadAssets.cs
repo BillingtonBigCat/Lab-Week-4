3d8227fd475ce25aaaed5f952a93096b6b84fed0
BillingtonBigCat <69779148+BillingtonBigCat@users.noreply.github.com>
2020-08-17 16:37:58 +1000

    70 percent complete

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -1,15 +1,25 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    [SerializeField]
+    private GameObject blueObj;
+    Vector3 redpos;
+    Vector3 bluepos;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        redpos = new Vector3(2.0f, 0.0f, 0.0f);
+        bluepos = new Vector3(-2.0f, 0.0f, 0.0f);
+        Instantiate(redObj, redpos,  Quaternion.identity);
+        Instantiate(blueObj, bluepos, Quaternion.identity);
+
     }
 
     // Update is called once per frame
